1ac02a6ca80fe6b9cd0efaef7f0bc11626cc90c2
Alvin <2421656763@qq.com>
2025-03-06 14:03:40 +0800

    13

--- dao/favorite.go
+++ dao/favorite.go
@@ -22,13 +22,14 @@ func NewFavoriteDaoByDB(db *gorm.DB) *FavoriteDao {
 
 // 根据id获取favorite
 func (dao *FavoriteDao) ListFavorite(uId uint) (favorite []*model.Favorite, err error) {
-	err = dao.DB.Model(&model.Favorite{}).Where("user_id=?", uId).Find(&favorite).Error
+	err = dao.DB.Model(&model.Favorite{}).Where("user_id = ?", uId).Find(&favorite).Error
 	return
 }
 
+// 检查收藏夹是否已经存在
 func (dao *FavoriteDao) FavoriteExistOrNot(pId, uId uint) (exist bool, err error) {
 	var count int64
-	err = dao.DB.Model(&model.Favorite{}).Where("product_id=?AND user_id=?", pId, uId).Count(&count).Error
+	err = dao.DB.Model(&model.Favorite{}).Where("product_id = ? AND user_id = ?", pId, uId).Count(&count).Error
 	if err != nil {
 		return false, err
 	}
@@ -38,10 +39,12 @@ func (dao *FavoriteDao) FavoriteExistOrNot(pId, uId uint) (exist bool, err error
 	return true, nil
 }
 
+// 创建新的收藏夹
 func (dao *FavoriteDao) CreateFavorite(in *model.Favorite) error {
 	return dao.DB.Model(&model.Favorite{}).Create(&in).Error
 }
 
+// 删除收藏夹记录
 func (dao *FavoriteDao) DeleteFavorite(uId, fId uint) error {
 	return dao.DB.Model(&model.Favorite{}).
 		Where("id = ? AND user_id= ?", fId, uId).Delete(&model.Favorite{}).Error
